748b71204ceae0bc9880a4943ec3a661b7c7b6b5
Italo Lelis <italohdc@gmail.com>
2018-02-21 17:28:28 -0300

    fixed launch files namespaces. fixed ar track bridge angle

--- src/ar_track_to_odom.py
+++ src/ar_track_to_odom.py
@@ -33,18 +33,18 @@ class ArTrackToOdom(object):
         pose.position.x = (origin.position.y + self.shift[1]) * self.mult[1]
         pose.position.y = (origin.position.x + self.shift[0]) * self.mult[0]
         pose.position.z = (origin.position.z + self.shift[2]) * self.mult[2]
-        # ori = tf_conversions.Quaternion()
-        # ori.x = pose.orientation.x
-        # ori.y = pose.orientation.y
-        # ori.z = pose.orientation.z
-        # ori.w = pose.orientation.w
-        # [r, p, y] = tf_conversions.transformations.euler_from_quaternion([ori.x, ori.y, ori.z, ori.w])
-        # y = y + math.pi
-        # quat = tf_conversions.transformations.quaternion_from_euler(r, p, y)
-        # pose.orientation.x = quat[0]
-        # pose.orientation.y = quat[1]
-        # pose.orientation.z = quat[2]
-        # pose.orientation.w = quat[3]
+        ori = tf_conversions.Quaternion()
+        ori.x = origin.orientation.x
+        ori.y = origin.orientation.y
+        ori.z = origin.orientation.z
+        ori.w = origin.orientation.w
+        [r, p, y] = tf_conversions.transformations.euler_from_quaternion([ori.x, ori.y, ori.z, ori.w])
+        y = math.pi/2 - y
+        quat = tf_conversions.transformations.quaternion_from_euler(r, p, y)
+        pose.orientation.x = quat[0]
+        pose.orientation.y = quat[1]
+        pose.orientation.z = quat[2]
+        pose.orientation.w = quat[3]
         pose.orientation = origin.orientation
         return pose
 
